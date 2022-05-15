a8a02e02360c8cb3b2c75bcdd0f4026c76afd73b
AshirRaza <80819500+AshirRaza@users.noreply.github.com>
2022-05-15 16:01:12 +0500

    Update Gesture_Controlled_Car.ino

--- Gesture_Controlled_Car/Gesture_Controlled_Car.ino
+++ Gesture_Controlled_Car/Gesture_Controlled_Car.ino
@@ -1,6 +1,17 @@
 
 
 BluetoothSerial SerialBT;
+#include <Dabble.h>
+#include <DabbleInputs.h>
+#include <SensorModule.h>
+#include <motorControls.h>
+
+#define CUSTOM_SETTINGS
+#define INCLUDE_SENSOR_MODULE
+#include <AFMotor.h>
+
+#define INCLUDE_DABBLEINPUTS_MODULE
+#define INCLUDE_MOTORCONTROL_MODULE
 
 void setup() {
   Serial.begin(115200);
